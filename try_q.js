ea09ebf2dd73535c5f418b06ae4e8d7ceccc8f5a
anglee <ang.lee@gmail.com>
2013-09-03 13:24:54 -0400

    try Q.fcall and delay

--- try_q.js
+++ try_q.js
@@ -1,13 +1,15 @@
 var evaluate1 = function () {
-    setTimeout(function() {
-        console.log("evaluate1");
-    }, 1000);
+    Q.fcall(function () { console.log("async call evaluate1 "); });
+    console.log("evaluate 1");
+    return "output from ev1";
 };
-var evaluate2 = function () {
-    setTimeout(function() {
-        console.log("evaluate2");
-    }, 500);
+var evaluate2 = function (input) {
+    console.log("input = ", input);
+    Q.fcall(function () { console.log("async call evaluate2 "); });
+    console.log("evaluate 2");
+    return "output from ev2";
 };
 
-evaluate1();
-evaluate2();
+Q.fcall(evaluate1)
+    .delay(1000)
+    .then(evaluate2);

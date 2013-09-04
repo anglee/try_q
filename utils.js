d77d9092e73e027815bd005d5b8bcc73029cd9b9
anglee <ang.lee@gmail.com>
2013-09-04 14:10:53 -0400

    trying Q

--- utils.js
+++ utils.js
@@ -1,7 +1,19 @@
-/**
- * Created with JetBrains WebStorm.
- * User: alee
- * Date: 9/4/13
- * Time: 9:13 AM
- * To change this template use File | Settings | File Templates.
- */
+window.utils = {
+    getPrettyTime: function () {
+        var date = new Date();
+        var minutes = date.getMinutes();
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        var seconds = date.getSeconds();
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        return date.getHours() + ":" + minutes + ":" + seconds;
+    },
+    log: function () {
+        var args = Array.prototype.slice.call(arguments);
+        args.splice(0, 0, "" + this.getPrettyTime() + " - ");
+        console.log.apply(console, args);
+    }
+};
